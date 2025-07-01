c019fd8db5a5384b01485bd530262c91b5d4e9e9
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-06-30 21:16:26 -0300

    add paquete principal

---
+++ screenmatch2/src/main/java/com/aluracursos/screenmatch2/principal/Principal.java
@@ -0,0 +1,76 @@
+package com.aluracursos.screenmatch2.principal;
+
+import com.aluracursos.screenmatch2.model.DatosSerie;
+import com.aluracursos.screenmatch2.model.DatosTemporadas;
+import com.aluracursos.screenmatch2.service.ConsumoAPI;
+import com.aluracursos.screenmatch2.service.ConvierteDatos;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = System.getenv("OMDB_API_KEY");
+    //private final String API_KEY = "TU-APIKEY-OMDB";
+    private final String SEPARADOR_APIKEY = "&apikey=";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraElMenu() {git s
+        var opcion = -1;
+        while (opcion != 0) {
+            var menu = """
+                    1 - Buscar series 
+                    2 - Buscar episodios
+                    3 - Mostrar series buscadas
+                                  
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+
+                case 0:
+                    System.out.println("Cerrando la aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
+    }
+
+    private DatosSerie getDatosSerie() {
+        System.out.println("Escribe el nombre de la serie que deseas buscar");
+        var nombreSerie = teclado.nextLine();
+        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + SEPARADOR_APIKEY + API_KEY);
+        System.out.println(json);
+        DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+        return datos;
+    }
+    private void buscarEpisodioPorSerie() {
+        DatosSerie datosSerie = getDatosSerie();
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= datosSerie.totalDeTemporadas(); i++) {
+            var json = consumoApi.obtenerDatos(URL_BASE + datosSerie.titulo().replace(" ", "+") + "&season=" + i + SEPARADOR_APIKEY + API_KEY);
+            DatosTemporadas datosTemporada = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+    }
+    private void buscarSerieWeb() {
+        DatosSerie datos = getDatosSerie();
+        System.out.println(datos);
+    }
+
+
+}

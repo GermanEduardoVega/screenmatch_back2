91558109f47eba82da9bff1c0a1be16dc407779a
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-06-30 23:22:59 -0300

    agregando constructores

--- screenmatch2/src/main/java/com/aluracursos/screenmatch2/model/Serie.java
+++ screenmatch2/src/main/java/com/aluracursos/screenmatch2/model/Serie.java
@@ -13,6 +13,62 @@ public class Serie {
     private String actores;
     private String sinopsis;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalDeTemporadas() {
+        return totalDeTemporadas;
+    }
+
+    public void setTotalDeTemporadas(Integer totalDeTemporadas) {
+        this.totalDeTemporadas = totalDeTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
     public Serie(DatosSerie datosSerie){    //referencia datosSerie para conseguir crear una serie del record
         this.titulo = datosSerie.titulo();
         this.totalDeTemporadas = datosSerie.totalDeTemporadas();

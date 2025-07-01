6299cf01832df7b679df5e850183d87a7204dfeb
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-06-30 23:14:48 -0300

    agregando la categoria

--- screenmatch2/src/main/java/com/aluracursos/screenmatch2/model/Categoria.java
+++ screenmatch2/src/main/java/com/aluracursos/screenmatch2/model/Categoria.java
@@ -1,16 +1,26 @@
 package com.aluracursos.screenmatch2.model;
 
-public enum Categoria {     //realiza la conversión entre la información de OMDB y la de nuestra aplicación
+public enum Categoria {
     ACCION("Action"),
     ROMANCE("Romance"),
     COMEDIA("Comedy"),
     DRAMA("Drama"),
     CRIMEN("Crime");
-    private String categoriaOmbd;
+    private String categoriaOmdb;
 
     //Constructor   //tratamiento para las categorias de nuestras series
-    Categoria(String categoriaOmbd){
-        this.categoriaOmbd = categoriaOmbd;
+    Categoria(String categoriaOmdb){
+
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {   //realiza la conversión entre la información de OMDB y la de nuestra aplicación
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
     }
 }
 
